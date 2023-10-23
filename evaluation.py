c42947a20dadb7c1c36e2f83ffe25aa2355fab17
Tobi <tobi.lingenberg@t-online.de>
2023-10-23 13:22:05 +0200

    submission test

--- evaluation.py
+++ evaluation.py
@@ -154,7 +154,7 @@ if __name__ == '__main__':
     class Config:
         data_dir = './data/'
         SCHEMA = os.path.join(data_dir, 'schemas/warm_up/schema.json')
-        num_episodes = 1
+        num_episodes = 3
 
         # Power outage probability:
         # p(outage|day) = 0.393% (modified to 1.97%)
