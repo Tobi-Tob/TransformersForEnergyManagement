ee3af7b18c6156ac95991943c32076d125324fd1
Tobi <tobi.lingenberg@t-online.de>
2023-10-27 21:32:50 +0200

    UnservedEnergyReward Test 6 no clip

--- rewards/custom_reward.py
+++ rewards/custom_reward.py
@@ -65,7 +65,7 @@ class UnservedEnergyReward(RewardFunction):
 
     def calculate(self, observations):
         """
-        Funktion 5 (higher outage (157), lower lr (3e-5))
+        Funktion 6 no clip
         """
         if self.simulation_time_steps is None:
             self.simulation_time_steps = self.env_metadata['simulation_time_steps']
@@ -97,11 +97,10 @@ class UnservedEnergyReward(RewardFunction):
                 e_loss_coef = building_metadata['electrical_storage']['loss_coefficient']
                 dhw_loss_coef = building_metadata['dhw_storage']['loss_coefficient']
 
-                energy_from_electrical_storage = np.round(np.clip((1 - e_loss_coef) * self.previous_electrical_storage[i] - electrical_storage[i],
-                                                                  a_min=0, a_max=np.inf) * ec * e_efficiency, decimals=4)
-                energy_from_dhw_storage = np.round(np.clip((1 - dhw_loss_coef) * self.previous_dhw_storage[i] - dhw_storage[i],
-                                                           a_min=0, a_max=np.inf) * dc, decimals=4)
-                # TODO vllt ohne clip
+                energy_from_electrical_storage = np.round(((1 - e_loss_coef) * self.previous_electrical_storage[i] - electrical_storage[i])
+                                                          * ec * e_efficiency, decimals=4)
+                energy_from_dhw_storage = np.round(((1 - dhw_loss_coef) * self.previous_dhw_storage[i] - dhw_storage[i]) * dc, decimals=4)
+
                 expected_energy = cooling_demand[i] + dhw_demand[i] + non_shiftable_load[i]
                 served_energy = energy_from_electrical_storage + energy_from_dhw_storage + solar_generation[i]  # info vllt als feature?
                 # TODO vllt reward signal von solar_generation loesen
